7b44fc52c00bec43b7926f9a0887be45c1d20e36
Abigail_pai <abigail.pai@roycetechnology.com>
2021-08-11 12:46:32 +0400

    get point and pong

--- assets/typescriptPaddles.ts
+++ assets/typescriptPaddles.ts
@@ -16,31 +16,106 @@ export class TypescriptPaddles extends Component {
     @property(Node)
     private paddle02: Node = null;
 
-    @integer
-    private point01: number = 0;
+    @property(Node)
+    private ball: Node = null;
 
-    @integer
-    private point02: number = 0;
+    @property(Node)
+    private point01: string = '0';
 
-    private positionXMouse: number
+    @property(Node)
+    private point02: string = '0';
+
+    private startGame: boolean = false
+    private positionXMouse: number 
+    private isUp: boolean = true 
 
     start () {
         // [3]
-
+        // for paddle
         systemEvent.on(SystemEvent.EventType.MOUSE_MOVE, this.onMouseMove, this)
+        // for ball
+        systemEvent.on(SystemEvent.EventType.MOUSE_DOWN, this.onMouseDown, this)
     }
 
 
     onMouseMove(event: EventMouse){
         let positionX:number = event.getLocationX()
         this.positionXMouse = positionX
-        this.point01 = 1
+    }
+
+    onMouseDown(event: EventMouse){
+        let mouseType = event.getButton();
+
+        // Identify the location of the button
+        let isBtnLeft = mouseType === EventMouse.BUTTON_LEFT
+
+        // Identify swiping direction of the ball
+        let mouseClick = event.getLocation();
+        let swipeToUp =  (mouseClick.y - this.ball.position.y) > 640
+
+        // start
+        this.startGame = true
+
+        if (isBtnLeft && swipeToUp) {
+            this.isUp = true
+        }else if(isBtnLeft && !swipeToUp) {
+            this.isUp = false
+        }
+    }
+    changeDirection(x: number, y: number) {
+        this.ball.setPosition(new Vec3(this.ball.position.x + x , this.ball.position.y + y , 0))
+        
+    }
+    getPoint(side:string) {
+        if(side === 'top'){
+            this.point01.components[1].string = this.point01.components[1].string * 1 + 1
+        }else {
+            this.point02.components[1].string = this.point02.components[1].string * 1 + 1
+        }
+    }
+    resetGame() {
+        this.startGame = false
+        this.ball.setPosition(new Vec3(0 , 0 , 0))
     }
 
     update (deltaTime: number) {
         // [4]
+
+        let isHit = this.paddle01.getPosition().x < this.ball.getPosition().x  && this.ball.getPosition().x < this.paddle01.getPosition().x + 50
+        let isHitWall = this.ball.getPosition().x === 480 || this.ball.getPosition().x === -480
+
         this.paddle01.setPosition(new Vec3(this.positionXMouse/2 - 480, 300, 0))
         this.paddle02.setPosition(new Vec3(this.positionXMouse/2 - 480, -300, 0))
+        console.log(this.ball.getPosition())
+        
+        if( isHitWall ) {
+            this.startGame = !this.startGame
+            this.changeDirection(-2, -2)
+
+        }else if(this.startGame && this.isUp) {
+            this.changeDirection(2, 2)
+            if(this.ball.getPosition().y === 320){
+                this.resetGame()
+                this.getPoint('bottom')
+            }else if(this.ball.getPosition().y === 270 && isHit) {
+                this.isUp = !this.isUp
+                console.log('pong')
+                this.changeDirection(2, -2)
+
+            }
+
+        }else if(this.startGame && !this.isUp) {
+            this.changeDirection(2, -2)
+            if(this.ball.getPosition().y===-320){
+                this.resetGame()
+                this.getPoint('top')
+            }else if(this.ball.getPosition().y === -270 && isHit) {
+                this.isUp = !this.isUp
+                console.log('pong')
+                this.changeDirection(2, 2)
+
+            }
+        }
 
     }
 }
